1e9b8816dc718102b290ac9fb8f62a06bf0627a9
Lucas <lucorscia@bell.net>
2019-03-08 15:52:50 -0500

    Améliorations de la page Catégorie

--- wp-content/themes/twentynineteen-exo7/template-parts/content/content-titre-cours.php
+++ wp-content/themes/twentynineteen-exo7/template-parts/content/content-titre-cours.php
@@ -14,16 +14,17 @@
 
 		<?php
 		$var = substr ( get_the_title() , 4 , 1  );
-		echo '<div class="unCours session'.$var.'">';          
 		echo '<a href="';
-		echo (get_permalink());
-		echo '"><h2 class="cours-title">';
+		echo (get_permalink());		
+		echo '"><div class="unCours session'.$var.'">';          
+
+		echo '<h2 class="cours-title">';
 		echo(substr ( get_the_title() , 8 ));
 		echo '</h2>';
 
 		echo '<h2 class="entry-title">';
 		echo(substr ( get_the_title() , 0 , 7 ));
-		echo '</h2></a></div>';
+		echo '</h2></div></a>';
 
 
 		//the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
