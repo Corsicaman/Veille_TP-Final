1b0748dbc8416504041f81b007a0de274bbe28b9
Lucas <lucorscia@bell.net>
2019-03-09 16:24:25 -0500

    Image mise en avant

--- wp-content/themes/twentynineteen-exo7/front-page.php
+++ wp-content/themes/twentynineteen-exo7/front-page.php
@@ -15,39 +15,33 @@ get_header();
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+
+
+<?php if (has_post_thumbnail( $post->ID ) ): ?>
+
+
+<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); 
+
+ endif; ?>
+
+
+
+
 			<?php
 			$query = new WP_Query( array( 'category_name' => 'evenement' ) );
-
-
-
-
 // The Loop
-if ( $query->have_posts() ) {
-	echo '<h1 id="titreAccueil" class="animTitre">Page d\'accueil personalisée</h1>';
-	echo '<h2 id="sousTitreAccueil" class="animTitre">Liste des évènements</h2>';
-	echo '<div id="grid">';
-	while ( $query->have_posts() ) {
-		$query->the_post();
-		get_template_part( 'template-parts/content/content', 'front-page' );
-	}
-	echo '</div>';
-	/* Restore original Post Data */
-	wp_reset_postdata();
-} else {
-	// no posts found
-}
-			// /* Start the Loop */
-			// while ( have_posts() ) :
-			// 	the_post();
-
-			// 	get_template_part( 'template-parts/content/content', 'front-page' );
-
-			// 	// If comments are open or we have at least one comment, load up the comment template.
-			// 	if ( comments_open() || get_comments_number() ) {
-			// 		comments_template();
-			// 	}
-
-			// endwhile; // End of the loop.
+		if ( $query->have_posts() ) {
+			echo '<h1 id="titreAccueil" class="animTitre">Page d\'accueil personalisée</h1>';
+			echo '<h2 id="sousTitreAccueil" class="animTitre">Liste des évènements</h2>';
+			echo '<div id="grid">';
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				get_template_part( 'template-parts/content/content', 'front-page' );
+			}
+			echo '</div>';
+			/* Restore original Post Data */
+			wp_reset_postdata();
+			}
 			?>
 
 		</main><!-- #main -->
