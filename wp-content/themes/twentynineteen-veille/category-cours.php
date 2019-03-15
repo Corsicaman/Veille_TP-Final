107084ad49075f6dfb84aeb6d6689592ed619b76
Lucas <lucorscia@bell.net>
2019-03-15 00:36:39 -0400

    Finalisation animations

--- wp-content/themes/twentynineteen-veille/category-cours.php
+++ wp-content/themes/twentynineteen-veille/category-cours.php
@@ -18,9 +18,7 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				?>
+				<h1 class="animTitre titreCours titreListe">Liste des cours du programme TIM</h1>
 			</header><!-- .page-header -->
 
 <div id="grid">
